1478271b7b794a632cfd89198061ba5d17e425cb
unknown <user@user-HP.(none)>
2012-08-04 03:59:28 -0400

    Map rasterization getting better but still not great.

--- lua/story/center.lua
+++ lua/story/center.lua
@@ -20,8 +20,6 @@ function _M:new(id,p)
 	self.__index = self    
 	return setmetatable({
 		_id=id,_point=p,
-		_minPoint = point:new(2,2),
-		_maxPoint = point:new(-2,-2),
 		_neighbors={},_borders={},_corners={}},
 		self)
 end
